afbc9d4a85507255aba524013bc3b17b47b0d584
arindom80 <d.arindom@gmail.com>
2014-12-03 09:54:35 +0800

    Update Bootstrap.java

--- sources/site/Bootstrap.java
+++ sources/site/Bootstrap.java
@@ -16,7 +16,7 @@ class Bootstrap {
             }
             public void handle(String target, Request baseRequest,
                 HttpServletRequest request, HttpServletResponse response)
-                throws IOExce ption, ServletException {
+                throws IOException, ServletException {
                 if (target.startsWith("/time")) {
                     response.setHeader("Content-Type", "text/html");
                     response.getWriter().write("<p>" + new Date());
